63ef0d3d354ecb2491fb20be4fdc637cd0f34671
Spencer Goodman <spencerwgoodman@icloud.com>
2018-07-10 16:53:06 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,4 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  post '/student' do
+    @student = Student.new(params[:student])
+
+    params[:student][:courses].each do |details|
+      Course.new(details)
+    end
+
+    @courses = Course.all
+
+    erb :student
+  end
 end
